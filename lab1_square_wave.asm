7fa3998338ef1f93c8e377f22712c7d5773f4a4a
hardikkhariwal297312 <128465167+hardikkhariwal297312@users.noreply.github.com>
2024-01-09 23:32:06 +0530

    Update lab1_square_wave.asm

--- lab1_square_wave.asm
+++ lab1_square_wave.asm
@@ -1,25 +1,22 @@
-//objective - program to add 2 8-bit numbers stored in 30h and 31h and store the result in 33h.
-
-//steps-
-// 1. fetch the numbers from the memory and store registers
-// 2. compute the sum and store it back in the memory
-
-
 ORG 0H
 LJMP MAIN
 ORG 100H
 	
 MAIN:
+SETB P1.4
+ACALL DELAY
+CLR P1.4
+ACALL DELAY
+SJMP MAIN
 
-ACALL ADD8
+DELAY:
+MOV R2,#255
 
-HERE: SJMP HERE
+DELAY1:
+MOV R1,#255
+HERE: DJNZ R1, HERE
+DJNZ R2, DELAY1
 
-ADD8:
-MOV A,30H //FETCHES THE VALUE PRESENT IN 30H AND TRANSFERS IT TO A
-MOV R1,31H 
-ADD A,R1 // A = A + R1
-MOV 33H,A
 RET
 
 END
